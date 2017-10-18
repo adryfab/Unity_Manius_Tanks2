cbab73d5701735dda1535b77057bf80c1b60947c
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-18 14:10:16 -0500

    Fondo y jugador

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float VelocidadJugador;
+
+    private Vector3 PosicionJugador;
+
+    // Use this for initialization
+    void Start ()
+    {
+        PosicionJugador = gameObject.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // movimiento horizontal
+        PosicionJugador.x += Input.GetAxis("Horizontal") * VelocidadJugador;
+        // movimiento vertical
+        PosicionJugador.y += Input.GetAxis("Vertical") * VelocidadJugador;
+        // actualiza el valor de la posicion del Transform del objeto Jugador
+        transform.position = PosicionJugador;
+    }
+}
