2822b2a5b3b0f499dd6e1bd44595d102fb467ab8
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-19 14:01:04 -0500

    Añadido Standard Assets

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerControl : MonoBehaviour {
 
     public float VelocidadJugador;
 
-    private Vector3 PosicionJugador;
+    //private Vector3 PosicionJugador;
     private Rigidbody2D rgb;
 
     // Use this for initialization
     void Start ()
     {
-        PosicionJugador = gameObject.transform.position;
+        //PosicionJugador = gameObject.transform.position;
         rgb = GetComponent<Rigidbody2D>();
     }
 	
@@ -27,28 +28,46 @@ public class PlayerControl : MonoBehaviour {
         MovimientoRGB();
     }
 
-    void MovimientoTransform()
-    {
-        // movimiento horizontal
-        PosicionJugador.x += Input.GetAxis("Horizontal") * VelocidadJugador;
-        // movimiento vertical
-        PosicionJugador.y += Input.GetAxis("Vertical") * VelocidadJugador;
-        // actualiza el valor de la posicion del Transform del objeto Jugador
-        transform.position = PosicionJugador;
-    }
+    //void MovimientoTransform()
+    //{
+    //    // movimiento horizontal
+    //    PosicionJugador.x += Input.GetAxis("Horizontal") * VelocidadJugador;
+    //    // movimiento vertical
+    //    PosicionJugador.y += Input.GetAxis("Vertical") * VelocidadJugador;
+    //    // actualiza el valor de la posicion del Transform del objeto Jugador
+    //    transform.position = PosicionJugador;
+    //}
 
-    void MovimientoRGB()
+    public void MovimientoRGB()
     {
         // movimiento horizontal
         float h = Input.GetAxis("Horizontal");
+        float hm = CrossPlatformInputManager.GetAxis("Horizontal");
+
         // movimiento vertical
         float v = Input.GetAxis("Vertical");
+        float vm = CrossPlatformInputManager.GetAxis("Vertical");
 
         Vector2 vel = new Vector2(rgb.velocity.x, rgb.velocity.y);
+
+        //añadir velocidad
         h *= VelocidadJugador;
+        hm *= VelocidadJugador;
         v *= VelocidadJugador;
-        vel.x = h;
-        vel.y = v;
+        vm *= VelocidadJugador;
+
+        //if (h > 0 || v > 0)
+        //{
+            vel.x = h;
+            vel.y = v;
+        //}
+        //else if (hm > 0 || vm > 0)
+        //{
+        //    vel.x = hm;
+        //    vel.y = vm;
+        //}
+
+        //Añadiendo la velocidad al rigidbody
         rgb.velocity = vel;
     }
 }
