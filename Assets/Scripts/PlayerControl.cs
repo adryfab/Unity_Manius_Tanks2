bd6ecd7d5f704a707cc738b6d32bb1d05abc1908
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-20 11:48:46 -0500

    Cambios minimos

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,29 +5,49 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerControl : MonoBehaviour {
 
-    public float VelocidadJugador;
+    //public float VelocidadJugador;
+    public float speed = 10;
 
     //private Vector3 PosicionJugador;
-    private Rigidbody2D rgb;
+    //private Rigidbody2D rgb;
+    private Rigidbody2D myBody;
+    private float hInput = 0;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         //PosicionJugador = gameObject.transform.position;
-        rgb = GetComponent<Rigidbody2D>();
+        //rgb = GetComponent<Rigidbody2D>();
+        //myBody = this.rigidbody2D;
+        myBody = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         //MovimientoTransform();
     }
 
     private void FixedUpdate()
     {
-        #if UNITY_ANDROID
-        MovimientoRGB();
+        #if !UNITY_ANDROID
+            Move(Input.GetAxisRaw("Horizontal"));
+        #else
+            Move(hInput);
         #endif
+    }
+
+    void Move(float horizontalInput)
+    {
+        Vector2 moveVel = myBody.velocity;
+        moveVel.x = horizontalInput * speed;
+        //moveVel.y = verticalInput * speed;
+        myBody.velocity = moveVel;
+    }
+
+    public void StartMoving(float horizontalInput)
+    {
+        hInput = horizontalInput;
     }
 
     //void MovimientoTransform()
@@ -40,36 +60,36 @@ public class PlayerControl : MonoBehaviour {
     //    transform.position = PosicionJugador;
     //}
 
-    public void MovimientoRGB()
-    {
-        // movimiento horizontal
-        float h = Input.GetAxisRaw("Horizontal");
-        //float hm = CrossPlatformInputManager.GetAxis("Horizontal");
+    //public void MovimientoRGB()
+    //{
+    //    // movimiento horizontal
+    //    float h = Input.GetAxisRaw("Horizontal");
+    //    //float hm = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        // movimiento vertical
-        float v = Input.GetAxisRaw("Vertical");
-        //float vm = CrossPlatformInputManager.GetAxis("Vertical");
+    //    // movimiento vertical
+    //    float v = Input.GetAxisRaw("Vertical");
+    //    //float vm = CrossPlatformInputManager.GetAxis("Vertical");
 
-        Vector2 vel = new Vector2(rgb.velocity.x, rgb.velocity.y);
+    //    Vector2 vel = new Vector2(rgb.velocity.x, rgb.velocity.y);
 
-        //añadir velocidad
-        h *= VelocidadJugador;
-        //hm *= VelocidadJugador;
-        v *= VelocidadJugador;
-        //vm *= VelocidadJugador;
+    //    //añadir velocidad
+    //    h *= VelocidadJugador;
+    //    //hm *= VelocidadJugador;
+    //    v *= VelocidadJugador;
+    //    //vm *= VelocidadJugador;
 
-        //if (h > 0 || v > 0)
-        //{
-            vel.x = h;
-            vel.y = v;
-        //}
-        //else if (hm > 0 || vm > 0)
-        //{
-        //    vel.x = hm;
-        //    vel.y = vm;
-        //}
+    //    //if (h > 0 || v > 0)
+    //    //{
+    //    vel.x = h;
+    //    vel.y = v;
+    //    //}
+    //    //else if (hm > 0 || vm > 0)
+    //    //{
+    //    //    vel.x = hm;
+    //    //    vel.y = vm;
+    //    //}
 
-        //Añadiendo la velocidad al rigidbody
-        rgb.velocity = vel;
-    }
+    //    //Añadiendo la velocidad al rigidbody
+    //    rgb.velocity = vel;
+    //}
 }
