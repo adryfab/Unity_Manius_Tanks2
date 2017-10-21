15183a88aa92de79e4e2d18e1abf31121c72abf8
adryfab <adry_fab@hotmail.com>
2017-10-20 22:09:01 -0500

    Moviendo al jugador

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,20 +8,47 @@ public class PlayerControl : MonoBehaviour {
     public float speed = 10;
 
     private Rigidbody2D myBody;
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
 
     void Start()
     {
         myBody = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
     }
 
     void Update()
-    {
-        
+    {        
+        Debug.Log("Vertical: " + CrossPlatformInputManager.GetAxis("Vertical").ToString() +
+            " - Horizontal: " + CrossPlatformInputManager.GetAxis("Horizontal").ToString());
+
+        playerMoving = false;
+
+        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0.5f || 
+            CrossPlatformInputManager.GetAxis("Horizontal") < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), 0f); 
+        }
+        if (CrossPlatformInputManager.GetAxis("Vertical") > 0.5f || 
+            CrossPlatformInputManager.GetAxis("Vertical") < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(0f, CrossPlatformInputManager.GetAxis("Vertical"));
+        }
+
+        anim.SetFloat("MoveX", CrossPlatformInputManager.GetAxis("Horizontal"));
+        anim.SetFloat("MoveY", CrossPlatformInputManager.GetAxis("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * speed;
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), 
+            CrossPlatformInputManager.GetAxis("Vertical")) * speed;
         myBody.AddForce(moveVec);
     }
 
