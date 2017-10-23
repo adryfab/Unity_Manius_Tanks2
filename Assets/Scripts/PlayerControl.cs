8e54842c6a1d92f09ff98112f93f8b528797e340
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-23 09:33:25 -0500

    Colores

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,17 +11,20 @@ public class PlayerControl : MonoBehaviour {
     private Animator anim;
     private bool playerMoving;
     private Vector2 lastMove;
+    private SpriteRenderer rend;
 
     void Start()
     {
         myBody = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+        CambiarColorPlayer();
     }
 
     void Update()
     {        
-        Debug.Log("Vertical: " + CrossPlatformInputManager.GetAxis("Vertical").ToString() +
-            " - Horizontal: " + CrossPlatformInputManager.GetAxis("Horizontal").ToString());
+        //Debug.Log("Vertical: " + CrossPlatformInputManager.GetAxis("Vertical").ToString() +
+        //    " - Horizontal: " + CrossPlatformInputManager.GetAxis("Horizontal").ToString());
 
         playerMoving = false;
 
@@ -52,4 +55,17 @@ public class PlayerControl : MonoBehaviour {
         myBody.AddForce(moveVec);
     }
 
+    private void CambiarColorPlayer()
+    {
+        //rend.color = new Color(1, 1, 0, 1); //yellow
+        //rend.color = new Color(0, 0, 1, 1); //blue
+        //rend.color = new Color(1, 0, 0, 1); //red
+        //rend.color = new Color(0, 0, 0, 1); //black
+        //rend.color = new Color(0, 0, 0, 0); //clear
+        //rend.color = new Color(0, 1, 1, 1); //cyan
+        //rend.color = new Color(0.5f, 0.5f, 0.5f, 1); //gray
+        //rend.color = new Color(0, 1, 0, 1); //green
+        //rend.color = new Color(1, 0, 1, 1); //magenta
+        //rend.color = new Color(1, 1, 1, 1); //white
+    }
 }
