38970714c8104c0b50be43a6957c25d99a973766
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-26 12:28:53 -0500

    Creando disparo en enemigo

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -6,6 +6,8 @@ using Polarith.AI.Move;
 public class EnemyControl : MonoBehaviour
 {
     public float MoveSpeed = 4; //Establecer velocidad de persecución
+    public Transform trans; //De donde sale el disparo
+    public GameObject disparo; //Prefab Disparo
 
     private AIMContext aimContext;
     private Rigidbody2D myBody;
@@ -16,6 +18,7 @@ public class EnemyControl : MonoBehaviour
         myBody = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponent<Animator>();
         aimContext = this.GetComponent<AIMContext>();
+        trans = GameObject.Find("Disparo").transform;
     }
 
     void Update ()
@@ -29,5 +32,17 @@ public class EnemyControl : MonoBehaviour
         Vector2 moveVec;
         moveVec = new Vector2(aimContext.DecidedDirection.x, aimContext.DecidedDirection.y) * MoveSpeed;
         myBody.AddForce(moveVec);
+    }
+
+    private void CrearDisparo(float posX, float posY, float posZ, Vector3 target)
+    {
+        GameObject DisparoCopia = Instantiate(disparo, trans.position, trans.rotation);
+        Rigidbody2D rb = DisparoCopia.GetComponent<Rigidbody2D>();
+        rb.AddForce(target * MoveSpeed, ForceMode2D.Impulse);
+    }
+
+    private void Disparar()
+    {
+
     }
 }
