54baa7c521838a66b5d80734d490ed3f04cff480
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-10-30 15:03:09 -0500

    Creando Menu Principal

---
+++ Assets/Scripts/MenuGameControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGameControl : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Escena01");
+    }
+
+}
