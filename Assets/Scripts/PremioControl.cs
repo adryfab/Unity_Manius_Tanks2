e497938b9216b5fadf02a3d638e3c214ee719835
Adriana F. Martínez C <adry_fab@hotmail.com>
2017-11-01 16:15:03 -0500

    Iniciando con premios

---
+++ Assets/Scripts/PremioControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PremioControl : MonoBehaviour
+{
+    public Image premio;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "DisparoPlayer")
+        {
+            
+        }
+    }
+}
